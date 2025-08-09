3597348d17163829f6da44015ad820d19a5a8110
dip <dipexplorerid23@gmail.com>
2025-08-09 20:29:51 +0530

    improve lawyer specific page

--- src/controllers/lawyer.controller.js
+++ src/controllers/lawyer.controller.js
@@ -2,6 +2,7 @@ const User = require("../models/user.model.js");
 const asyncHandler = require("../utils/asyncHandler");
 const apiError = require("../utils/apiError");
 const apiResponse = require("../utils/apiResponse");
+const LawyerProfile = require("../models/lawyer.model.js");
 
 const getLawyers = asyncHandler(async (req, res) => {
     const lawyers = await User.find({});
@@ -11,7 +12,13 @@ const getLawyers = asyncHandler(async (req, res) => {
 });
 
 const viewLawyer = asyncHandler(async (req, res) => {
-    const lawyer = await User.findById(req.params.id);
+    const lawyer = await User.findById(req.params.id).populate({
+        path: "lawyerProfile",
+        model: LawyerProfile,
+        select: "bio specialization licenseNumber experience city state languagesSpoken availableSlots fees ",
+    });
+    console.log(lawyer);
+
     if (!lawyer) {
         return res.status(404).send("Lawyer not found");
     }
@@ -19,15 +26,7 @@ const viewLawyer = asyncHandler(async (req, res) => {
     if (req.accepts("html")) {
         return res.render("pages/lawyer-profile", { lawyer });
     } else {
-        return res
-            .status(200)
-            .json(
-                new apiResponse(
-                    200,
-                    lawyer,
-                    "Lawyer profile fetched successfully"
-                )
-            );
+        return res.status(200).json(new apiResponse(200,lawyer,"Lawyer profile fetched successfully"));
     }
 });
 
