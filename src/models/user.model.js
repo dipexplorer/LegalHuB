38d2326bbc01a772e05bfa481eb09e0db28e489b
DeepanshuT25 <deepanshuchauhan1211@gmail.com>
2025-08-24 03:28:15 +0530

    set-up google authentication

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,45 +3,40 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema(
-    {
-        name: {
-            type: String,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-        },
-        role: {
-            type: String,
-            enum: ["user", "lawyer", "admin"],
-            default: "user",
-        },
-        profilePicture: {
-            type: String,
-            default:
-                "https://cdn.vectorstock.com/i/1000v/51/87/student-avatar-user-profile-icon-vector-47025187.jpg",
-        },
-        lawyerProfile: {
-            type: Schema.Types.ObjectId,
-            ref: "LawyerProfile",
-            default: null,
-        },
-        isActive: {
-            type: Boolean,
-            default: true,
-        },
-        resetToken: String,
-        resetTokenExpires: Date,
-    },
-    {
-        timestamps: true,
-    }
+{
+name: { type: String, trim: true },
+email: {
+type: String,
+required: true,
+unique: true,
+lowercase: true,
+trim: true
+},
+role: {
+type: String,
+enum: ["user", "lawyer", "admin"],
+default: "user"
+},
+profilePicture: {
+type: String,
+default: "https://cdn.vectorstock.com/i/1000v/51/87/student-avatar-user-profile-icon-vector-47025187.jpg"
+},
+oauth: {
+provider: { type: String, enum: ['google', 'github', 'facebook', null], default: null },
+providerId: { type: String, default: null }
+},
+lawyerProfile: {
+type: Schema.Types.ObjectId,
+ref: "LawyerProfile",
+default: null
+},
+isActive: { type: Boolean, default: true },
+resetToken: String,
+resetTokenExpires: Date
+},
+{ timestamps: true }
 );
 
 userSchema.plugin(passportLocalMongoose);
-
 module.exports = mongoose.model("User", userSchema);
+
